00761c6e4c3e303729950d918989991e7000734d
Monish <mayekarmonish875@gmail.com>
2024-02-23 23:28:56 +0530

    Second Commit

--- backend/index1.js
+++ backend/index1.js
@@ -6,7 +6,7 @@ const {MongoClient} = require('mongodb');
 app.use(express.urlencoded());
 app.use(bodyParser.json());
 
-let [db,client,collection] = '';
+// let [db,client,collection] = '';
 async function connectToDatabase() {
     try {
         client = await MongoClient.connect('mongodb+srv://Monish:mmonish875@cluster0.7pfxpj7.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -18,32 +18,6 @@ async function connectToDatabase() {
 }
 
 connectToDatabase().then(async() => {
-    collection = db.collection('emails');
-    
-    app.post("/email",async(req,res)=>{
-        let email = `${req.body.email}`.split('@')[0];
-        let data = await collection.findOne({[email]:{ $exists : true }});
-        if (data==null) {
-            let acknowledgement = await collection.insertOne(
-            {
-                [email]:
-                {
-                    'workspaces':['Workspace 1'],
-                    'cards':
-                    {
-                    'cards_name':['Card 1',0],
-                    'cards_desc':['Card Description',0],
-                    'cards_title':['Card Title',0],
-                    },
-                    'tasks':['Sip a Coffee',0]
-                }
-            })
-            res.json({acknowledgement : acknowledgement.insertedId})
-        }
-        else{
-            res.json(data[`${email}`])
-        }
-    })
 
     app.get("/loveit",(req,res)=>{
         res.send("Say hi");
