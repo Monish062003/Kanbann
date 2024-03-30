c0026a5fec2088c59e4ed5df0d99fd531741a707
Monish <mayekarmonish875@gmail.com>
2024-03-31 02:42:08 +0530

    Django building up

--- backend1/backend1/settings.py
+++ backend1/backend1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@m+y0c1(q*ns%j*62iy7up$)464u-sklt2gbe^1$9p920zq!a@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
