542b7dd59cbf04cad5799a9547c582319c82d774
Monish <mayekarmonish875@gmail.com>
2024-03-31 16:07:16 +0530

    Try 6

--- backend1/build.sh
+++ backend1/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Ensure Python 3.8 is available
-if ! command -v python3.8 &>/dev/null; then
-    echo "Error: Python 3.8 is not installed or not available."
+# Ensure Python 3.9 is available
+if ! command -v python3.9 &>/dev/null; then
+    echo "Error: Python 3.9.9 is not installed or not available."
     exit 1
 fi
 
 # Create virtual environment
-python3.8 -m venv venv
+python3.9 -m venv venv
 
 # Activate virtual environment
 source venv/bin/activate || { echo "Error: Failed to activate virtual environment."; exit 1; }
