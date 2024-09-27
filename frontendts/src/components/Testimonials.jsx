cb1cbd35bae760ad17d773fd598ab319650b34af
Monish <mayekarmonish875@gmail.com>
2024-09-27 07:49:54 +0530

    Responsive Made

--- frontendts/src/components/Testimonials.jsx
+++ frontendts/src/components/Testimonials.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import { videoList } from "../json/videoList";
 import "../Css/testimonials.scss";
+import leftButton from "../Images/left_button.png";
+import rightButton from "../Images/right_button.png";
 
 function Testimonials() {
   const [Videos, setVideos] = useState([]);
+  const videoRefs = useRef([]);
 
   useEffect(() => {
     let videoArray = [];
@@ -13,10 +16,18 @@ function Testimonials() {
     setVideos(videoArray);
   }, [videoList]);
 
-  useEffect(() => {}, [Left, Right]);
+  useEffect(() => {
+    videoRefs.current = videoRefs.current
+      .slice(0, Videos.length)
+      .map((_, i) => videoRefs.current[i] || React.createRef());
+  }, [Videos.length]);
 
   function Left() {
     let videoArray = [...Videos];
+    const video_elements = videoRefs.current;
+    video_elements.forEach((element, index) => {
+      element.classList.add("animateleft");
+    });
     for (let index = 0; index < videoList.length; index++) {
       const element = videoList[index];
       if (element.title === Videos[Videos.length - 1].title) {
@@ -31,6 +42,13 @@ function Testimonials() {
 
   function Right() {
     let videoArray = [...Videos];
+    const video_elements = videoRefs.current;
+    video_elements.forEach((element, index) => {
+      if (element) {
+        element.classList.add("animateright");
+        console.log(element);
+      }
+    });
     for (let index = 0; index < videoList.length; index++) {
       const element = videoList[index];
       if (element.title === Videos[0].title) {
@@ -42,6 +60,7 @@ function Testimonials() {
       }
     }
   }
+
   return (
     <div className="testimonials-section">
       <h1>what our clients say</h1>
@@ -51,25 +70,42 @@ function Testimonials() {
       </p>
       <div className="video-section">
         <button className="nav-buttons" onClick={Left}>
-          Left
+          <img src={leftButton} alt="left_nav" />
         </button>
         <div className="videopanel">
-          {Videos.map((video, index) => (
-            <video
-              key={video.id}
-              className={
-                index === Math.floor(Videos.length / 2)
-                  ? "mainframe"
-                  : "videoframe"
-              }
-              src={video.src}
-              title={video.title}
-              controls
-            ></video>
-          ))}
+          {Videos.map((video, index) => {
+            let animations = "";
+            switch (index) {
+              case 0:
+                animations =
+                  (window.innerWidth < 768 ? "leftpanel " : "") +
+                  "videoframe zligning";
+                break;
+
+              case 1:
+                animations = "mainframe";
+                break;
+
+              case 2:
+                animations =
+                  (window.innerWidth < 768 ? "rightpanel " : "") +
+                  "videoframe zligning ";
+                break;
+            }
+            return (
+              <video
+                ref={(el) => (videoRefs.current[index] = el)}
+                key={video.id}
+                className={animations}
+                controls={index === 1}
+                src={video.src}
+                title={video.title}
+              ></video>
+            );
+          })}
         </div>
         <button className="nav-buttons" onClick={Right}>
-          Right
+          <img src={rightButton} alt="right_nav" />
         </button>
       </div>
     </div>
